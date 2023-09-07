d1ce2142549d9e633a1c33c679536b39033add2a
Muhammad-Ali-Muhammad <freelancing.jobs.2023@gmail.com>
2023-09-07 13:20:37 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
  * Describtion: print a quote using write function
- * write (int fd.const void *buf.size_t count)
+ *    ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: 1 (not success)
- */
-int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ * Return: 1 (error).
+*/
 
-	write(1.quo.59);
+
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
