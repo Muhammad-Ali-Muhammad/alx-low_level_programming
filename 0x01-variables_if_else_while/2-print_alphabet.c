ea505a5ea2da9929769e87e755a48207099b94fa
Muhammad-Ali-Muhammad <freelancing.jobs.2023@gmail.com>
2023-09-08 13:12:13 -0700

    Task 12

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -11,10 +10,8 @@
 int main(void)
 {
 	char low;
-
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 		putchar('\n');
-
 	return (0);
 }
